a2f9fe0f0ca591ebbee6b541c2b8334315c2e9d6
Alan Ponte <103460929+alanponte@users.noreply.github.com>
2024-06-17 11:30:07 -0700

    more test cases

--- tests/kandae/test_kandae.py
+++ tests/kandae/test_kandae.py
@@ -5,7 +5,9 @@ from kandae.kandae import smallest_sum_subarr
 
 @pytest.mark.parametrize(
     "input, expected", [
-        ([3, -4, 2, -3, -1, 7, -5], ((-6, 1, 4)))
+        ([3, -4, 2, -3, -1, 7, -5], (-6, 1, 4)),
+        ([1], (1, 0, 0)),
+        ([1, 2], (1, 0, 0))
     ]
 )
 def test_kandae(input, expected):
